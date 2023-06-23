460a0ca442e29609dc4c46414846214fc08e794c
Onan Thairu <thairuonan@gmail.com>
2023-06-23 08:54:33 +0300

    fix: display profile picture for currently logged in user.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -26,7 +26,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image 
-          src={session?.user.image}
+          src="/assets/images/logo.svg"
           alt="Prompts Logo"
           width={30}
           height={30}
@@ -48,7 +48,7 @@ const Nav = () => {
 
                 <Link href="/profile">
                   <Image
-                    src="/assets/images/logo.svg"
+                    src={session?.user.image}
                     width={37}
                     height={37}
                     className="rounded-full"
