be793a764d76ba2c823f764d09fb538133234460
Onan Thairu <thairuonan@gmail.com>
2023-07-08 13:54:42 +0300

    fix: remove url to profile.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -40,7 +40,7 @@ const Nav = () => {
           {
             session?.user ? (
               <div className="flex gap-3 md:gap-5">
-                <Link href="/create-prompt" className="black_btn">
+                <Link href="#" className="black_btn">
                   Create Post
                 </Link>
 
